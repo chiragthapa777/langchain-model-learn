f8fb6fe4bd6a9c10f5dec70672fca7e2df7e8926
Proshore <proshore@Proshores-MacBook-Air.local>
2025-02-18 08:31:53 +0545

    embedding vector and doing semantic search

--- 2.ChatModels/1_chatmodel_openai.py
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-4")
+model = ChatOpenAI(model="gpt-4") # currently 4o mini is the cheapest model
 
 
 result = model.invoke("what is the capital of nepal?")
